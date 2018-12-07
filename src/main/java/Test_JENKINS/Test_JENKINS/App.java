5a92a973db0d4428b3ffc001b27b788d452952e6
HUAH KOK YU <5004240068@pglivevid.com>
2018-12-07 14:57:06 +0800

    Jenkins test first commit

---
+++ src/main/java/Test_JENKINS/Test_JENKINS/App.java
@@ -0,0 +1,14 @@
+package Test_JENKINS.Test_JENKINS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is Jenkins build test!!");
+    }
+}
